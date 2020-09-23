2f2476810489b49114d56e6e5d70b687df5a6d51
LeeJuhae <tlatmsdlwngo@naver.com>
2020-09-23 20:52:11 +0900

    Add LandingPage

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+const style = {
+	"marginTop":"20px",
+}
+
+const LandingPage = () => {
+	return (
+		<div>
+			<Typography variant="h1" style={style}>
+				KuberKuber
+			</Typography>
+		</div>
+	);
+}
+export default LandingPage;
